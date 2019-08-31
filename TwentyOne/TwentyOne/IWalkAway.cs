9f0194496381e0c08618ba3ac82ae08f2de82360
Edmund D <52433772+Iowa20@users.noreply.github.com>
2019-08-31 12:16:14 -0400

    updated

---
+++ TwentyOne/TwentyOne/IWalkAway.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    interface IWalkAway //When naming an interface always start with a capital "I" just like "IWalkAway" the WalkAway starts with "I".
+    {
+        void WalkAway(Player player);
+
+
+    }
+}
