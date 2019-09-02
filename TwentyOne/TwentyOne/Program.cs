717e419a2432fda623da94ebc679eeb7a42c62d8
Edmund D <52433772+Iowa20@users.noreply.github.com>
2019-09-01 21:18:19 -0400

    update

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -2,6 +2,8 @@
 using Casino.TwentyOne;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -19,7 +21,20 @@ namespace TwentyOne
 
             Console.WriteLine("Welcome to the Grand Hotel and Casino. Let's start by telling me your name. ");
             string playerName = Console.ReadLine();
-
+            if(playerName.ToLower() == "admin")
+                {
+                    List<ExceptionEntity> Exeptions = ReadExceptions();
+                    foreach (var exception in Exeptions)
+                    {
+                        Console.Write(exception.Id + " | ");
+                        Console.Write(exception.ExceptionType + " | ");
+                        Console.Write(exception.ExceptionMessage + " | ");
+                        Console.Write(exception.TimeStamp + " | ");
+                        Console.WriteLine();
+                    }
+                Console.Read();
+                return;
+                }
 
             bool validAnswer = false;
             int bank = 0;
@@ -50,15 +65,18 @@ namespace TwentyOne
                     {
                         game.Play();
                     }
-                    catch (FraudException)
+                    catch (FraudException ex)
                     {
-                        Console.WriteLine("Security! Kick this person out.");
+                        Console.WriteLine(ex.Message);
+                        UpdateDbWithExcception(ex);
                         Console.ReadLine();
                         return;
                     }
-                    catch (Exception)
+                    catch (Exception ex)
                     {
                         Console.WriteLine("An Error occured. Please contact your System Administrator.");
+                        UpdateDbWithExcception(ex);
+
                         Console.ReadLine();
                         return;
                     }
@@ -74,5 +92,65 @@ namespace TwentyOne
             Console.ReadLine();
 
         }
+
+        private static void UpdateDbWithExcception(Exception ex)
+        {
+            string connectionString = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=TwentyOneGame;
+                                        Integrated Security=True;Connect Timeout=30;Encrypt=False;
+                                        TrustServerCertificate=False;ApplicationIntent=ReadWrite;
+                                        MultiSubnetFailover=False";
+
+            string queryString = @"INSERT INTO (ExceptionType, ExceptionMessage, TimeStamp) VALUES
+                                  (@ExceptionType, @ExceptionMessage, @TimeStamp)";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.Add("@ExceptionType", SqlDbType.VarChar);
+                command.Parameters.Add("@ExceptionMessage", SqlDbType.VarChar);
+                command.Parameters.Add("@TimeStamp", SqlDbType.DateTime);
+
+                command.Parameters["@ExceptionType"].Value = ex.GetType().ToString();
+                command.Parameters["@ExceptionMessage"].Value = ex.Message;
+                command.Parameters["@TimeStamp"].Value = DateTime.Now;
+
+                connection.Open();
+                command.ExecuteNonQuery(); // error here: not passing through SQL Db
+                connection.Close();
+            }
+        }
+        private static List<ExceptionEntity> ReadExceptions()
+        {
+            string connectionString = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=TwentyOneGame;
+                                        Integrated Security=True;Connect Timeout=30;Encrypt=False;
+                                        TrustServerCertificate=False;ApplicationIntent=ReadWrite;
+                                        MultiSubnetFailover=False";
+
+            string queryString = @"Select Id, ExceptionType, ExceptionMessage, TimeStamp From Exceptions";
+            List<ExceptionEntity> Exceptions = new List<ExceptionEntity>();
+
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    ExceptionEntity exception = new ExceptionEntity();
+                    exception.Id = Convert.ToInt32(reader["Id"]);
+                    exception.ExceptionType = reader["ExceptionType"].ToString();
+                    exception.ExceptionMessage = reader["ExceptionMessage"].ToString();
+                    exception.TimeStamp = Convert.ToDateTime(reader["TimeStamp"]);
+                    Exceptions.Add(exception);
+                }
+                connection.Close();
+            }
+
+            return Exceptions;
+
+        }
+
+
      }
 }
