d86e190453d5c11d8f0c86a524766e55121dbaa3
Edmund D <52433772+Iowa20@users.noreply.github.com>
2019-09-12 21:43:07 -0400

    update

--- CarInsurance/CarInsurance/Properties/AssemblyInfo.cs
+++ CarInsurance/CarInsurance/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1a11c4c3-0317-4bea-bb85-f108af57083a")]
+[assembly: Guid("3b318158-f0d0-4c02-874e-309aace3c2fc")]
 
 // Version information for an assembly consists of the following four values:
 //
